98057409fdefbe38a293c6c49c4d0f12170799ce
shobeedev <moha.saoud22@gmail.com>
2025-12-29 19:57:05 +0100

    Headers of math utilities

---
+++ include/math.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   math.h                                                 |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2025/12/29 19:21:44 by shobeedev               shobee4ever      */
+/*   Updated: 2025/12/29 19:56:08 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+int tt_max(int nbr1,int nbr2);
+float tt_max(float nbr1,float nbr2);
+double tt_max(double nbr1,double nbr2);
+int tt_min(int nbr1,int nbr2);
+float tt_min(float nbr1,float nbr2);
+double tt_min(double nbr1,double nbr2);
+int tt_sum(int nbr1,int nbr2);
+float tt_sum(float nbr1,float nbr2);
+double tt_sum(double nbr1,double nbr2);
+float tt_avg(float sum,int nbr);
+double tt_avg(double sum,int nbr);
+int tt_pow(int base,int exp);
+float tt_pow(float base,int exp);
+int tt_sqrt(int nbr);
+int tt_fact(int nbr);
