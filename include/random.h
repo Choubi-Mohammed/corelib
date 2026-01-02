8a4c7cb1d5e87905a29f48e24c170dd0478f8665
shobeedev <moha.saoud22@gmail.com>
2026-01-02 19:54:23 +0100

    Headers of random utilities

--- include/random.h
+++ include/random.h
@@ -6,11 +6,12 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/01/02 17:17:49 by shobeedev               shobee4ever      */
-/*   Updated: 2026/01/02 19:29:33 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/01/02 19:47:06 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifdef RANDOM_H
+#ifndef RANDOM_H
+#include <string>
+using namespace std;
 #define RANDOM_H
 int tt_random_int();
 int tt_random_range(int from,int to);
