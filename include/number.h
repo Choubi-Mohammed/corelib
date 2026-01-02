11d931088195dd639adb0722999e07baa81ff1eb
shobeedev <moha.saoud22@gmail.com>
2026-01-02 19:53:59 +0100

    Headers of number utilities

--- include/number.h
+++ include/number.h
@@ -6,12 +6,10 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2025/12/29 17:33:43 by shobeedev               shobee4ever      */
-/*   Updated: 2025/12/29 18:43:37 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2025/12/31 18:29:21 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifdef NUMBER_H
-#define NUMBER_H
+#pragma once
 bool tt_is_prime(int nbr);
 bool tt_is_even(int nbr);
 bool tt_is_odd(int nbr);
@@ -25,4 +23,3 @@ double tt_floor(double nbr);
 float tt_ceil(float nbr);
 double tt_ceil(double nbr);
 
-#endif
