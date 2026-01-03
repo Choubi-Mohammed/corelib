4a1ad40793766d9139a2070b52c6844005dc15a1
shobeedev <moha.saoud22@gmail.com>
2026-01-03 11:17:34 +0100

    add swap number to header

--- include/number.h
+++ include/number.h
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2025/12/29 17:33:43 by shobeedev               shobee4ever      */
-/*   Updated: 2025/12/31 18:29:21 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/01/03 11:16:22 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 #pragma once
@@ -22,4 +22,5 @@ float tt_floor(float nbr);
 double tt_floor(double nbr);
 float tt_ceil(float nbr);
 double tt_ceil(double nbr);
+void swap_nbr(int &num1,int &num2);
 
